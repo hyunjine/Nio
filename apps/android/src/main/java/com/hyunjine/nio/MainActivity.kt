a0e86946023bf70bd5cbb6e9e7d5910e34ac187a
hyunjine <thevlakk1@gmail.com>
2025-09-04 20:40:34 +0900

    feat: MainActivity
    - coil 라이브러리 추가
    - 기획 UI 구현

--- apps/android/src/main/java/com/hyunjine/nio/MainActivity.kt
+++ apps/android/src/main/java/com/hyunjine/nio/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.hyunjine.nio
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.hyunjine.nio.ui.theme.NioTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +35,7 @@ class MainActivity : ComponentActivity() {
             NioTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "옷",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,16 +46,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier = modifier) {
+        Text(
+            text = name
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("https://picsum.photos/400/200")
+                .crossfade(true)
+                .build(),
+//            placeholder = painterResource(R.drawable.placeholder),
+            contentDescription = "하하",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .clip(RoundedCornerShape(16.dp)),
+            onSuccess = { Log.d("winter", "굳") },
+            onError = { Log.d("winter", "${it.result.throwable}") }
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NioTheme {
-        Greeting("Android")
+        Greeting("옷")
     }
 }
