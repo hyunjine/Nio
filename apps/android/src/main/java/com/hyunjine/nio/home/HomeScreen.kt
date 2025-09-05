f9d578e6ce77553c3d647993b7c1fd7fede1c768
hyunjine <thevlakk1@gmail.com>
2025-09-05 23:46:04 +0900

    feat: MainActivity
    - HomeScreen 컴포저블 함수로 변경

---
+++ apps/android/src/main/java/com/hyunjine/nio/home/HomeScreen.kt
@@ -0,0 +1,59 @@
+package com.hyunjine.nio.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+    ) {
+        HomeComponent("옷")
+    }
+}
+
+@Composable
+fun HomeComponent(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = name,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("https://picsum.photos/400/200")
+                .crossfade(true)
+                .build(),
+//            placeholder = painterResource(R.drawable.placeholder),
+            contentDescription = "하하",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .clip(RoundedCornerShape(16.dp))
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HomeComponent("옷")
+}
