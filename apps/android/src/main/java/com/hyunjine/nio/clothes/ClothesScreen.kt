19f924de4b390e08b26628d54b7e10eff760e0fc
hyunjine <thevlakk1@gmail.com>
2025-09-09 19:56:25 +0900

    feat: ClothesScreen 옷 추가 버튼 UI 배치

--- apps/android/src/main/java/com/hyunjine/nio/clothes/ClothesScreen.kt
+++ apps/android/src/main/java/com/hyunjine/nio/clothes/ClothesScreen.kt
@@ -1,26 +1,31 @@
 package com.hyunjine.nio.clothes
 
-import android.widget.ImageView
-import androidx.compose.foundation.background
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.SemanticsProperties.Shape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,7 +35,6 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.hyunjine.nio.clothes.model.ClothesItemModel
-import com.hyunjine.nio.home.HomeComponent
 
 @Composable
 fun ClothesScreen(
@@ -44,6 +48,7 @@ fun ClothesGrid(
     modifier: Modifier = Modifier,
     viewModel: ClothesViewModel = viewModel()
 ) {
+
     val items by viewModel.clothes.collectAsStateWithLifecycle()
     ClothesGrid(
         modifier = modifier,
@@ -56,14 +61,28 @@ fun ClothesGrid(
     modifier: Modifier = Modifier,
     clothes: List<ClothesItemModel>
 ) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
-        verticalArrangement = Arrangement.spacedBy(6.dp),
-        modifier = modifier
-    ) {
-        items(clothes) { model ->
-            ClothesItem(model)
+    Box {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalArrangement = Arrangement.spacedBy(6.dp),
+            modifier = modifier
+        ) {
+            items(clothes) { model ->
+                ClothesItem(model)
+            }
+        }
+        FloatingActionButton(
+            shape = CircleShape,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 8.dp)
+            ,
+            onClick = {
+
+            }
+        ) {
+            Icon(Icons.Default.Add, contentDescription = null)
         }
     }
 }
