402143df7273923f481be115afbed42ea24966cc
hyunjine <thevlakk1@gmail.com>
2025-09-11 23:54:46 +0900

    feat: ClothesScreen 옷 추가 바텀시트 틀 구현

--- apps/android/src/main/java/com/hyunjine/nio/clothes/ClothesScreen.kt
+++ apps/android/src/main/java/com/hyunjine/nio/clothes/ClothesScreen.kt
@@ -1,12 +1,14 @@
 package com.hyunjine.nio.clothes
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -14,18 +16,26 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.semantics.SemanticsProperties.Shape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -78,12 +88,11 @@ fun ClothesGrid(
                 .align(Alignment.BottomEnd)
                 .padding(bottom = 8.dp)
             ,
-            onClick = {
-
-            }
+            onClick = {  }
         ) {
             Icon(Icons.Default.Add, contentDescription = null)
         }
+        MyBottomSheetSample()
     }
 }
 
@@ -109,6 +118,45 @@ fun ClothesItem(
             fontSize = 12.sp,
             color = Color.Black
         )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyBottomSheetSample() {
+    var openBottomSheet by remember { mutableStateOf(true) }
+
+    val sheetState = rememberModalBottomSheetState()
+
+//    Scaffold(
+//        floatingActionButton = {
+//            FloatingActionButton(onClick = { openBottomSheet = true }) {
+//                Icon(Icons.Default.Add, contentDescription = "Open Sheet")
+//            }
+//        }
+//    ) { padding ->
+//        Box(Modifier.wrapContentSize().padding(padding)) {
+//            Text("메인 화면 컨텐츠")
+//        }
+//    }
+
+    if (openBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { openBottomSheet = false },
+            sheetState = sheetState
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text("바텀시트 내용", style = MaterialTheme.typography.titleMedium)
+                Spacer(Modifier.height(8.dp))
+                Button(onClick = { openBottomSheet = false }) {
+                    Text("닫기")
+                }
+            }
+        }
     }
 }
 
